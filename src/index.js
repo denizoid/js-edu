92e223f5eb8986df9db5a5cb25012bdd9bdbd1a8
Denis Ahinski <ahinski.denis@gmail.com>
2019-10-08 23:24:57 +0300

    feat: add task

--- src/index.js
+++ src/index.js
@@ -5,10 +5,23 @@
  * @returns number of weeks needed for finish education
  */
 module.exports = function getTimeForEducation(
-    focus = 'family', 
-    knowsProgramming = true,
-    config = {family: 4}
-    ) {
-      return 0;
-  };
+  focus = 'family', 
+  knowsProgramming = true,
+  config = {family: 4}
+  ) {
+    let result = 0;
+    let master_speed = 0.375;
+    let basic_speed = 1;
+    if (knowsProgramming) {
+      if (300/(master_speed*config[focus]) % 1 > 0) return Math.floor(300/(master_speed*config[focus]))+1;
+      else return Math.floor(300/(master_speed*config[focus]));
+    }
+    else {
+      if (300/(master_speed*config[focus]) % 1 > 0) result += Math.floor(300/(master_speed*config[focus]))+1;
+      else result += Math.floor(300/(master_speed*config[focus]));
+    }
+    if (500/(basic_speed*config[focus]) % 1 >= 0.5) result += Math.floor(500/(basic_speed*config[focus]))+1;
+    else result += Math.floor(500/(basic_speed*config[focus]));
+    return result;
+};
   
